b300c44b1ed194693325b6378ce874e6b8884658
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2021-02-26 11:16:54 -0500

    Convert Calculator from class to function component

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CalcKey({ calcKey, onClick }) {
 	return (
@@ -8,70 +8,57 @@ function CalcKey({ calcKey, onClick }) {
 	);
 }
 
-class Calculator extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			displayNum: "0",
-		};
-		this.handleClick = this.handleClick.bind(this);
-	}
+function Calculator() {
+	const [displayNum, setDisplayNum] = useState("0");
 
-	updateNumber(numString) {
-		const { displayNum } = this.state;
+	const updateNumber = (numString) => {
 		if (displayNum === "0") {
-			this.setState({
-				displayNum: numString,
-			});
+			setDisplayNum(numString);
 		} else {
-			this.setState({
-				displayNum: displayNum + numString,
-			});
+			setDisplayNum(displayNum + numString);
 		}
-	}
+	};
 
-	updateOperator(operString) {
+	const updateOperator = (operString) => {
 		//this should be string
 		console.log(operString);
-	}
+	};
 
-	checkString(string) {
+	const checkString = (string) => {
 		string === "0"
-			? this.updateNumber(string)
+			? updateNumber(string)
 			: Number(string)
-			? this.updateNumber(string)
-			: this.updateOperator(string);
-	}
+			? updateNumber(string)
+			: updateOperator(string);
+	};
 
-	handleClick(e) {
-		this.checkString(e.target.textContent);
-	}
+	const handleClick = (e) => {
+		checkString(e.target.textContent);
+	};
 
-	render() {
-		const calcKeys = [
-			["C", "+/-", "%", "/"],
-			["7", "8", "9", "*"],
-			["4", "5", "6", "-"],
-			["1", "2", "3", "+"],
-			["color", "0", ".", "="],
-		];
+	const calcKeys = [
+		["C", "+/-", "%", "/"],
+		["7", "8", "9", "*"],
+		["4", "5", "6", "-"],
+		["1", "2", "3", "+"],
+		["color", "0", ".", "="],
+	];
 
-		const calcKeyboard = calcKeys.map((row, idx) => (
-			<div className={"calcRow calcRow_" + idx} key={"calcRow" + idx}>
-				<CalcKey calcKey={row[0]} onClick={(e) => this.handleClick(e)} />
-				<CalcKey calcKey={row[1]} onClick={(e) => this.handleClick(e)} />
-				<CalcKey calcKey={row[2]} onClick={(e) => this.handleClick(e)} />
-				<CalcKey calcKey={row[3]} onClick={(e) => this.handleClick(e)} />
-			</div>
-		));
+	const calcKeyboard = calcKeys.map((row, idx) => (
+		<div className={"calcRow calcRow_" + idx} key={"calcRow" + idx}>
+			<CalcKey calcKey={row[0]} onClick={(e) => handleClick(e)} />
+			<CalcKey calcKey={row[1]} onClick={(e) => handleClick(e)} />
+			<CalcKey calcKey={row[2]} onClick={(e) => handleClick(e)} />
+			<CalcKey calcKey={row[3]} onClick={(e) => handleClick(e)} />
+		</div>
+	));
 
-		return (
-			<div className={"calculator"}>
-				<div className={"calcDisplay"}>{this.state.displayNum}</div>
-				{calcKeyboard}
-			</div>
-		); //return
-	} //render
+	return (
+		<div className={"calculator"}>
+			<div className={"calcDisplay"}>{displayNum}</div>
+			{calcKeyboard}
+		</div>
+	); //return
 } //Calculator
 
 export default Calculator;
